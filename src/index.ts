318d25119e529a38b102b574b39fc800cbd9eb9a
chokoty <chokokty0816@gmail.com>
2023-12-11 15:58:57 +0900

    06-elysia-state-and-decorate

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,12 @@ const app = new Elysia()
         "/",
         () => "Hello Bun dev, I am gonna build build RESTFUL API with Elysia "
     )
-    .state("verson", 1)
+    // .state("verson", 1)
+    .state({
+        id: 1,
+        verson: 1,
+        email: "sample@gmail.com",
+    })
     .decorate("getDate", () => new Date())
     .get("post/:id", ({ params: { id } }) => {
         return { id: id, title: "learn bun" };
@@ -13,34 +18,49 @@ const app = new Elysia()
     // .post("/post", (context) => {
     //     return context;
     // })
-    .post("/post", ({ body, set }) => {
+    .post("/post", ({ body, set, store }) => {
+        console.log(store);
         set.status = 201; // 403 forbidden
         return body;
     })
     .get("track/*", () => {
         return "track";
     })
-    .get("tracks/", () => {
-        return new Response(
-            JSON.stringify({
-                tracks: [
-                    {
-                        id: 1,
-                        title: "Hello",
-                    },
-                    {
-                        id: 2,
-                        title: "World",
-                    },
-                ],
-            }), // body
-            {
-                headers: {
-                    "Content-Type": "application/json",
+    .get("tracks/", ({ store, getDate }) => {
+        // return new Response(
+        // JSON.stringify({
+        //     tracks: [
+        //         {
+        //             id: 1,
+        //             title: "Hello",
+        //         },
+        //         {
+        //             id: 2,
+        //             title: "World",
+        //         },
+        //     ],
+        // }), // body
+        // {
+        //     headers: {
+        //         "Content-Type": "application/json",
+        //     },
+        //     status: 200,
+        // } // response
+        // );
+        console.log(store.verson);
+        console.log(getDate());
+        return {
+            tracks: [
+                {
+                    id: 1,
+                    title: "Hello",
                 },
-                status: 200,
-            } // response
-        );
+                {
+                    id: 2,
+                    title: "World",
+                },
+            ],
+        };
     })
     .listen(3000);
 
