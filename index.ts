ccb8816e3fd072366cd8443c4f5d49ff95dfb7b2
chokoty <chokokty0816@gmail.com>
2023-12-11 13:27:23 +0900

    03-routes

--- index.ts
+++ index.ts
@@ -2,8 +2,20 @@ import figlet from "figlet";
 
 const server = Bun.serve({
     fetch(req) {
-        const body = figlet.textSync("Bunny!");
-        return new Response(body);
+        const url = new URL(req.url);
+
+        if (url.pathname === "/") {
+            const body = figlet.textSync("Bunny!");
+            return new Response(body);
+        }
+        if (url.pathname === "/about") {
+            return new Response("About page");
+        }
+        if (url.pathname === "/contact") {
+            return new Response("Contact page");
+        }
+
+        return new Response("Not found", { status: 404 });
     },
     port: 3000,
 });
